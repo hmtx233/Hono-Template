14930c3f1f79952e0ac2700f0eee8c75e86e86b9
Raj Mazumder <rajmazumder27.08.2001@gmail.com>
2024-11-19 09:47:15 +0530

    fix(runner): fix development server exports

--- src/app/development.ts
+++ src/app/development.ts
@@ -1,3 +1,10 @@
 /** @notice library imports */
 /// Local imports
-export * from "./application";
+import app from "./application";
+import { Environments } from "@/config";
+
+/// Running app
+export default {
+  fetch: app.fetch,
+  port: Environments.PORT,
+};
